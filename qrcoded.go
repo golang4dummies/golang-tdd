88ab304ed4b7d84a26573a406d768490d02843bc
Roberto Zagni <rz70rz70@gmail.com>
2014-10-26 18:21:12 +0100

    Corrected import statement to use () instead of {}

--- qrcoded.go
+++ qrcoded.go
@@ -1,17 +1,17 @@
 package main
 
-import {
+import (
 	"fmt"
 	"io/ioutil"
-	}
+	)
 
 func main() {
-	fmt.Println("Hello QR Code")
+	fmt.Println("Hello QR Code");	fmt.Println("GenerateQRCode is empty")
 	
 	qrcode := GenerateQRCode("555-2368")
 	ioutil.WriteFile("qrcode.png", qrcode, 0644)
 }
 
-func GenerateQRCode(code string) [] byte {
+func GenerateQRCode(code string) []byte {
 	return nil
 }
