28204df02d22a2cfb3642000a7998649b888bc97
Roberto Zagni <rz70rz70@gmail.com>
2014-10-28 21:43:36 +0100

    Auto-formatted with <go fmt>.

--- qrcoded.go
+++ qrcoded.go
@@ -3,15 +3,16 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
-	)
+)
 
 func main() {
-	fmt.Println("Hello QR Code");
-	
-	qrcode := GenerateQRCode("555-2368");	fmt.Printf("GenerateQRCode is %v", qrcode)
+	fmt.Println("Hello QR Code")
+
+	qrcode := GenerateQRCode("555-2368")
+	fmt.Printf("GenerateQRCode is %v", qrcode)
 	ioutil.WriteFile("qrcode.png", qrcode, 0644)
 }
 
 func GenerateQRCode(code string) []byte {
-	return []byte {0xFF}
+	return []byte{0xFF}
 }
