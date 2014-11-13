7cbae08b8bbbb43e2f7edd75b0f33888885e98fe
Roberto Zagni <rz70rz70@gmail.com>
2014-11-14 00:05:39 +0100

    Refactor - Reduce the amount of useless tests.

--- qrcoded_test.go
+++ qrcoded_test.go
@@ -6,18 +6,16 @@ import (
 	"image/png"
 )
 
-func TestGenerateQRCodeReturnsValue(t *testing.T) {
+
+func TestGenerateQRCodeGeneratesPNG(t *testing.T) {
+
 	buffer := new(bytes.Buffer)
 	GenerateQRCode(buffer, "555-2368")
-
+	
 	if buffer.Len() == 0 {
 		t.Errorf("Generated QR code lenght is zero")
 	}
-}
-
-func TestGenerateQRCodeGeneratesPNG(t *testing.T) {
-	buffer := new(bytes.Buffer)
-	GenerateQRCode(buffer, "555-2368")
+	
 	_, err := png.Decode(buffer)
 	
 	if err != nil {
